d0b439b96c2b3bcc5f4f995bb18e18a61ed6b33a
lzl <805298331@qq.com>
2017-12-22 17:13:15 +0800

    init

---
+++ src/components/register.vue
@@ -0,0 +1,8 @@
+<template>
+    <div>
+        <h1>德州扑克游戏大厅</h1>
+        <p>欢迎来到lzl的德州扑克游戏大厅，祝您游戏愉快</p>
+        <button>已有账号，马上进入大厅</button>
+        <button>还没账号，现在就去注册</button>
+    </div>
+</template>
